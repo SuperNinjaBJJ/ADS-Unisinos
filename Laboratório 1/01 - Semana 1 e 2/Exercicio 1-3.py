ea40435ceba9cdef2a7c2dc819814e94f0e18cde
Gabriel Rodrigues <gabriel.rteixeira99@gmail.com>
2025-08-11 15:19:25 -0300

    add exercicio 1-3

---
+++ Laboratório 1/01 - Semana 1 e 2/Exercicio 1-3.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite a sua altura: "))
+print("Sua idade é: ", idade)
+print("Seu nome é: ", nome)
+print("Sua altura é: ", altura)
+print("Obrigado!")
